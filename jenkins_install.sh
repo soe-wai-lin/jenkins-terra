248e6122f1f4ea12e3ab69e0809b2744822125bd
swl <swl@gmail.com>
2025-07-17 21:09:33 +0700

    add docker install

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -138,19 +138,13 @@ sudo systemctl enable sonar.service
 
 ### Install Docker ###
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y ca-certificates curl gnupg lsb-release 
-sudo mkdir -p /etc/apt/keyrings
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null  
-
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker $USER
-sudo usermod -aG docker Jenkin
-sudo newgrp docker
-
+sudo apt update -y
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt -y install docker-ce docker-ce-cli containerd.io 
+sudo snap install docker -y
+sudo systemctl start docker
+sudo systemctl enable --now docker
+sudo usermod -aG docker jenkins
