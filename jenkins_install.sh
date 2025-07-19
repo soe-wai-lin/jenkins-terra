7fc1889987999ababb798c989921bb5e3f27d420
swl <swl@gmail.com>
2025-07-19 08:50:25 +0700

    add docker install

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -43,6 +43,19 @@ sudo apt update -y
 
 sudo apt-get install trivy -y
 
+### Install Docker ###
+
+sudo apt update -y
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt -y install docker-ce docker-ce-cli containerd.io 
+sudo snap install docker -y
+sudo systemctl start docker
+sudo systemctl enable --now docker
+sudo usermod -aG docker jenkins
+
 ### Add PostgresSQL repository ###
 
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
@@ -136,15 +149,4 @@ sudo systemct restart sonar.service
 # Enable the service to start on boot
 sudo systemctl enable sonar.service
 
-### Install Docker ###
 
-sudo apt update -y
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update -y
-sudo apt -y install docker-ce docker-ce-cli containerd.io 
-sudo snap install docker -y
-sudo systemctl start docker
-sudo systemctl enable --now docker
-sudo usermod -aG docker jenkins
